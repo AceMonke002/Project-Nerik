825b83ed29b4e2706dc6e17dd8da77bc15ebb4cf
AceMonke002 <149847454+AceMonke002@users.noreply.github.com>
2024-06-03 09:35:37 -0500

    Jump Implemented
    Jump has been implemented to both keyboard(space) and gamepad(ps4 X button)
    
    Discovered Issues: when turning in the air even with rigidbody locks the model spin out of control

--- Assets/Scripts/CharacterMovementController.cs
+++ Assets/Scripts/CharacterMovementController.cs
@@ -9,6 +9,16 @@ public class CharacterMovementController : MonoBehaviour
 
     public float groundDrag;
 
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("KeyBinding")]
+    public KeyCode jumpKeykeyboard = KeyCode.Space;
+    public KeyCode jumpKeygamepad = KeyCode.Joystick1Button1;
+
+
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
@@ -27,6 +37,8 @@ public class CharacterMovementController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        
+        readyToJump = true;
     }
 
     private void Update()
@@ -57,13 +69,30 @@ public class CharacterMovementController : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if((Input.GetKey(jumpKeykeyboard) || Input.GetKey(jumpKeygamepad)) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        if(grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if(!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+        
     }
 
     private void SpeedControl()
@@ -76,5 +105,16 @@ public class CharacterMovementController : MonoBehaviour
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
